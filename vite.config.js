2e9ccaed6b9eb7260ebd40ea4847b9d7c7605b9c
Dahlia Foo <dahliafoo@gmail.com>
2022-07-06 10:37:23 +1000

    small updates

--- vite.config.js
+++ vite.config.js
@@ -3,5 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  plugins: [react()]
+  base: '/nft-batch-bidder/',
+  plugins: [react()],
 })
